2e2f044d3d974579b21fd883beb1047ff98690c0
eltaj <eltajnabiev007@gmail.com>
2024-07-11 00:57:00 +0300

    M

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -12,8 +12,6 @@ router.register(
 )
 
 urlpatterns = [
-    path('api-token-auth/', views.obtain_auth_token),
     path('v1/api-token-auth/', views.obtain_auth_token),
-    path('', include(router.urls)),
-    path('v1/', include(router.urls)),
+    path('v1/', include(router.urls))
 ]
